f9c841b0a6233d68be28e97af1d581a6db022ada
NS-Sheam <123sheamfeni@gmail.com>
2024-01-19 14:04:05 +0600

    Add jwt-decode library and update login functionality

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,19 +1,29 @@
 import { Button } from "antd";
 import { useForm } from "react-hook-form";
 import { useLoginMutation } from "../redux/features/auth/authApi";
+import { useAppDispatch } from "../redux/hooks";
+import { setUser } from "../redux/features/auth/authSlice";
 
 const Login = () => {
+  const dispatch = useAppDispatch();
+
   const { register, handleSubmit } = useForm({
     defaultValues: {
       id: "A-0001",
       password: "admin123",
     },
   });
-  const [login, { data, isError, isLoading, isSuccess }] = useLoginMutation();
-  console.log(data);
+  const [login, { isError, isLoading, isSuccess }] = useLoginMutation();
 
-  const onSubmit = (data) => {
-    console.log(data);
+  const onSubmit = async (data) => {
+    const userInfo = {
+      id: data.id,
+      password: data.password,
+    };
+    const res = await login(userInfo).unwrap();
+    console.log(res);
+
+    dispatch(setUser({ user: {}, token: res.data.accessToken }));
   };
 
   return (
