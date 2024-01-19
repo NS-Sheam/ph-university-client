30725e9e38972de0e592f160bc819b26b9bc1583
NS-Sheam <123sheamfeni@gmail.com>
2024-01-19 11:09:49 +0600

    sidebar item generator done

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,7 +1,8 @@
 import { Layout, Menu } from "antd";
 const { Header, Content, Footer, Sider } = Layout;
 import { Outlet } from "react-router-dom";
-import { adminSidebarItems } from "../../router/admin.routes";
+import { sidebarItemsGenerator } from "../../utils/sidebarItemsGenerator";
+import { adminPaths } from "../../router/admin.routes";
 
 const MainLayout = () => {
   return (
@@ -31,7 +32,7 @@ const MainLayout = () => {
           theme="dark"
           mode="inline"
           defaultSelectedKeys={["4"]}
-          items={adminSidebarItems}
+          items={sidebarItemsGenerator(adminPaths, "admin")}
         />
       </Sider>
       <Layout>
--- src/router/admin.routes.tsx
+++ src/router/admin.routes.tsx
@@ -62,22 +62,22 @@ export const adminPaths = [
 //   return acc;
 // }, []);
 
-export const adminSidebarItems = adminPaths.reduce((acc: TSidebarItem[], item) => {
-  if (item.path && item.name) {
-    acc.push({
-      key: item.name,
-      label: <NavLink to={`/admin/${item.path}`}>{item.name}</NavLink>,
-    });
-  }
-  if (item.children) {
-    acc.push({
-      key: item.name,
-      label: item.name,
-      children: item.children.map((child) => ({
-        key: child.name,
-        label: <NavLink to={`/admin/${child.path}`}>{child.name}</NavLink>,
-      })),
-    });
-  }
-  return acc;
-}, []);
+// export const adminSidebarItems = adminPaths.reduce((acc: TSidebarItem[], item) => {
+//   if (item.path && item.name) {
+//     acc.push({
+//       key: item.name,
+//       label: <NavLink to={`/admin/${item.path}`}>{item.name}</NavLink>,
+//     });
+//   }
+//   if (item.children) {
+//     acc.push({
+//       key: item.name,
+//       label: item.name,
+//       children: item.children.map((child) => ({
+//         key: child.name,
+//         label: <NavLink to={`/admin/${child.path}`}>{child.name}</NavLink>,
+//       })),
+//     });
+//   }
+//   return acc;
+// }, []);
