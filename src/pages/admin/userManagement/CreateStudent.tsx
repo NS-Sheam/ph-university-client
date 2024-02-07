b1edeebe31978afda1dc33a79fe7cdb2d91b9426
NS-Sheam <123sheamfeni@gmail.com>
2024-02-07 11:00:07 +0600

    Add image upload functionality to CreateStudent form

--- src/pages/admin/userManagement/CreateStudent.tsx
+++ src/pages/admin/userManagement/CreateStudent.tsx
@@ -1,7 +1,7 @@
-import { FieldValues, SubmitHandler } from "react-hook-form";
+import { Controller, FieldValues, SubmitHandler } from "react-hook-form";
 import PHForm from "../../../components/form/PHForm";
 import PHInput from "../../../components/form/PHInput";
-import { Button, Col, Divider, Row } from "antd";
+import { Button, Col, Divider, Form, Input, Row } from "antd";
 import PHSelect from "../../../components/form/PHSelect";
 import { bloodGroupOptions, genderOptions } from "../../../constants/global";
 import PHDatePicker from "../../../components/form/PHDatePicker";
@@ -109,7 +109,8 @@ const CreateStudent = () => {
 
     const formData = new FormData();
     formData.append("data", JSON.stringify(studentData));
-    addStudent(formData);
+    formData.append("file", data.image);
+    await addStudent(formData);
   };
 
   return (
@@ -184,6 +185,27 @@ const CreateStudent = () => {
                 options={bloodGroupOptions}
                 name="bloodGroup"
                 label="Blood Group"
+              />
+            </Col>
+            <Col
+              span={24}
+              md={{ span: 12 }}
+              lg={{ span: 8 }}
+            >
+              <Controller
+                name="image"
+                render={({ field: { onChange, value, ...field } }) => (
+                  <Form.Item label="Image">
+                    <Input
+                      type="file"
+                      value={value?.fileName}
+                      {...field}
+                      onChange={(e) => {
+                        onChange(e.target.files?.[0]);
+                      }}
+                    />
+                  </Form.Item>
+                )}
               />
             </Col>
           </Row>
